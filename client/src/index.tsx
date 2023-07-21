0b6b85cf097f77c5e3a98b7909018f090c684405
UnknownEquineCoder <21IMC10066@fh-krems.ac.at>
2023-07-21 19:08:32 +0200

    🔨 Added router wrapper

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,6 +5,7 @@
 import { render } from "solid-js/web";
 import "./index.css";
 import App from "./App";
+import { Router } from "@solidjs/router";
 
 const root = document.getElementById("root");
 
@@ -14,4 +15,11 @@ if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
   );
 }
 
-render(() => <App />, root!);
+render(
+  () => (
+    <Router>
+      <App />
+    </Router>
+  ),
+  root!
+);
