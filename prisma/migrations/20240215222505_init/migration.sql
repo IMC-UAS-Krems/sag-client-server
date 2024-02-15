13f689c35a1e1b3a3d8a80f1acd24d5da9e4b777
bobokrut <egor-evlampiev@mail.ru>
2024-02-16 00:16:44 +0100

    feat: unified migrations for local and remote databases

---
+++ prisma/migrations/20240215222505_init/migration.sql
@@ -0,0 +1,123 @@
+-- CreateEnum
+CREATE TYPE "ConsumerLevel" AS ENUM ('NO_ACCESS', 'LOW_ACCESS', 'MID_ACCESS', 'HIGH_ACCESS');
+
+-- CreateEnum
+CREATE TYPE "ProducerLevel" AS ENUM ('NO_ACCESS', 'LOW_ACCESS', 'MID_ACCESS', 'HIGH_ACCESS');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "writePrivilege" "ProducerLevel" NOT NULL DEFAULT 'NO_ACCESS',
+    "readPrivilege" "ConsumerLevel" NOT NULL DEFAULT 'NO_ACCESS',
+    "municipalityId" TEXT,
+    "orgId" TEXT,
+    "projectId" TEXT,
+    "registered" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "documents" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "version" INTEGER NOT NULL DEFAULT 1,
+    "authorId" TEXT NOT NULL,
+    "projectId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+
+    CONSTRAINT "documents_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "municipalities" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+
+    CONSTRAINT "municipalities_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "organisations" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "municipalityId" TEXT NOT NULL,
+    "verified" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+
+    CONSTRAINT "organisations_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "projects" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "orgId" TEXT NOT NULL,
+    "minWritePrivilege" "ProducerLevel" NOT NULL DEFAULT 'HIGH_ACCESS',
+    "minReadPrivilege" "ConsumerLevel" NOT NULL DEFAULT 'HIGH_ACCESS',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+
+    CONSTRAINT "projects_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_id_key" ON "users"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "documents_id_key" ON "documents"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "municipalities_id_key" ON "municipalities"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "municipalities_name_key" ON "municipalities"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "organisations_id_key" ON "organisations"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "organisations_name_key" ON "organisations"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "projects_id_key" ON "projects"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "projects_name_key" ON "projects"("name");
+
+-- AddForeignKey
+ALTER TABLE "users" ADD CONSTRAINT "users_municipalityId_fkey" FOREIGN KEY ("municipalityId") REFERENCES "municipalities"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "users" ADD CONSTRAINT "users_orgId_fkey" FOREIGN KEY ("orgId") REFERENCES "organisations"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "users" ADD CONSTRAINT "users_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "projects"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "documents" ADD CONSTRAINT "documents_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "documents" ADD CONSTRAINT "documents_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "projects"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "organisations" ADD CONSTRAINT "organisations_municipalityId_fkey" FOREIGN KEY ("municipalityId") REFERENCES "municipalities"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "projects" ADD CONSTRAINT "projects_orgId_fkey" FOREIGN KEY ("orgId") REFERENCES "organisations"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
