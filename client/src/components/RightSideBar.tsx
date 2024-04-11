76bcad355b1d9ebc35c180c92a358a7c50fe1de0
AnamariaZvonaru <anazvonaru@gmail.com>
2024-04-11 21:30:25 +0200

    introduced tailwind css

--- client/src/components/RightSideBar.tsx
+++ client/src/components/RightSideBar.tsx
@@ -2,32 +2,35 @@ import { For } from "solid-js";
 import { errors } from "@store/index";
 
 export function RightSideBar() {
+  console.log(errors())
   return (
     <div class="right-column" style={{ overflow: "auto" }}>
       {errors().length > 0 ? (
-        <table style={{ width: "100%" }}>
-          <thead>
+        <table style={{ width: "100%" }} class='table-auto border-separate border border-slate-500'>
+          <thead            class="border-b border-neutral-200 font-medium dark:border-white/10">
             <tr>
-              <th style="textAlign: left">Line</th>
-              <th style="textAlign: left">Column</th>
-              <th style="textAlign: left">Error</th>
+              <th class="text-left text-lg border-b-2 border-gray-800 border border-slate-500" >Line</th>
+              <th class="text-left text-lg border-b-2 border-gray-800 border border-slate-500">Column</th>
+              <th class="text-left text-lg border-b-2 border-gray-800 border border-slate-500">Error</th>
             </tr>
           </thead>
           <tbody>
-            <For each={errors()}>
-              {(error, index) => (
-                <tr>
-                  <td>{error.line_start}</td>
-                  <td>{error.column_start}</td>
-                  <td>{error.error}</td>
-                </tr>
-              )}
-            </For>
+          <For each={errors()}>
+  {(error, index) => (
+    <tr>
+      <td  class="border border-slate-300">{error.line_start}</td>
+      <td class="border border-slate-300">{error.column_start}</td>
+      <td class="border border-slate-300">{error.error}</td>
+    </tr>
+  )}
+</For>
           </tbody>
         </table>
       ) : (
-        <p style="textAlign: center">No errors found!</p>
+        <p class='text-center'>No errors found!</p>
       )}
     </div>
   );
 }
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./client/index.html",
+    "./client/src/**/*.{js,jsx,ts,tsx,html}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
