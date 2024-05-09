2ec4664c88952376a7da3055805da965b3e3d287
AnamariaZvonaru <anazvonaru@gmail.com>
2024-05-09 18:56:01 +0200

    fix: table border

--- client/src/components/RightSideBar.tsx
+++ client/src/components/RightSideBar.tsx
@@ -6,21 +6,21 @@ export function RightSideBar() {
   return (
     <div class="right-column" style={{ overflow: "auto" }}>
       {errors().length > 0 ? (
-        <table style={{ width: "100%" }} class='table-auto border-separate border border-slate-500'>
-          <thead            class="border-b border-neutral-200 font-medium dark:border-white/10">
+        <table style={{ width: "100%" }} class='border-solid border-2 border-black'>
+          <thead class="">
             <tr>
-              <th class="text-left text-lg border-b-2 border-gray-800 border border-slate-500" >Line</th>
-              <th class="text-left text-lg border-b-2 border-gray-800 border border-slate-500">Column</th>
-              <th class="text-left text-lg border-b-2 border-gray-800 border border-slate-500">Error</th>
+              <th class="text-left text-lg border-solid border-2 border-black" >Line</th>
+              <th class="text-left text-lg border-solid border-2 border-black">Column</th>
+              <th class="text-left text-lg border-solid border-2 border-black">Error</th>
             </tr>
           </thead>
           <tbody>
           <For each={errors()}>
   {(error, index) => (
     <tr>
-      <td  class="border border-slate-300">{error.line_start}</td>
-      <td class="border border-slate-300">{error.column_start}</td>
-      <td class="border border-slate-300">{error.error}</td>
+      <td  class="text-center border-solid border-2 border-black">{error.line_start}</td>
+      <td class="text-center border-solid border-2 border-black">{error.column_start}</td>
+      <td class="border-spacing-2 border-solid border-2 border-black">{error.error}</td>
     </tr>
   )}
 </For>
