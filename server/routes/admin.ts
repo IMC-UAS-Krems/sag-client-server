04d8e93e7665c24a747f0d7b2f0c2ab49da95bf4
bobokrut <egor-evlampiev@mail.ru>
2024-08-05 08:34:35 +0200

    fix: removed `guard()` since it badly affects `eden` client

--- server/routes/admin.ts
+++ server/routes/admin.ts
@@ -1,17 +1,14 @@
 import { authMiddleware } from "@server/middleware";
 import { Elysia, t } from "elysia";
 
-export const admin = new Elysia({ prefix: "/admin" }).guard(
+export const admin = new Elysia({ prefix: "/admin" }).get(
+  "/users",
+  async ({ set, userId }) => {
+    set.status = 200;
+    return "Users";
+  },
   {
     beforeHandle: authMiddleware,
+    detail: { tags: ["admin"] },
   },
-  (app) =>
-    app.get(
-      "/users",
-      async ({ set, userId }) => {
-        set.status = 200;
-        return "Users";
-      },
-      { detail: { tags: ["admin"] } },
-    ),
 );
--- server/routes/api.ts
+++ server/routes/api.ts
@@ -19,126 +19,129 @@ export const api = new Elysia({ prefix: "/api" })
     },
     { detail: { tags: ["api"] } },
   )
-  .guard({ beforeHandle: authMiddleware }, (app) =>
-    app
-      .post(
-        "/compile",
-        async ({ log, set, body: { code }, userId }) => {
-          const compiled = await fetch(`${COMPILER_URL}/compile`, {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              source: code,
-              user_id: userId,
-            }),
-          });
-
-          const data = await compiled.json();
-
-          if (!compiled.ok || data.status == "error") {
-            log.error(`${data.error}`);
-            return data;
-          }
-
-          set.status = 200;
-          log.info(`Data ${data} compiled.`);
-          return data;
+  .post(
+    "/compile",
+    async ({ log, set, body: { code }, userId }) => {
+      const compiled = await fetch(`${COMPILER_URL}/compile`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
         },
-        {
-          body: t.Object({
-            code: t.String(),
-          }),
-          response: t.Union([
-            t.Object({ status: t.Literal("ok"), url: t.String() }),
-            t.Object({ status: t.Literal("error"), errors: t.Array(t.Any()) }),
-            t.Object({ status: t.Literal("error"), error: t.String() }),
-          ]),
-          detail: { tags: ["api"] },
-        },
-      )
-      .post(
-        "/check",
-        async ({ log, set, userId, body: { code } }) => {
-          const compiled = await fetch(`${COMPILER_URL}/check`, {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              source: code,
-              user_id: userId,
-            }),
-          });
+        body: JSON.stringify({
+          source: code,
+          user_id: userId,
+        }),
+      });
 
-          const data = await compiled.json();
+      const data = await compiled.json();
 
-          if (!compiled.ok || data.status == "error") {
-            log.error(`${data.error}`);
-            return data;
-          }
+      if (!compiled.ok || data.status == "error") {
+        log.error(`${data.error}`);
+        return data;
+      }
 
-          set.status = 200;
-          log.info(`Data ${data} compiled.`);
-          return data;
+      set.status = 200;
+      log.info(`Data ${data} compiled.`);
+      return data;
+    },
+    {
+      body: t.Object({
+        code: t.String(),
+      }),
+      beforeHandle: authMiddleware,
+      response: t.Union([
+        t.Object({ status: t.Literal("ok"), url: t.String() }),
+        t.Object({ status: t.Literal("error"), errors: t.Array(t.Any()) }),
+        t.Object({ status: t.Literal("error"), error: t.String() }),
+      ]),
+      detail: { tags: ["api"] },
+    },
+  )
+  .post(
+    "/check",
+    async ({ log, set, userId, body: { code } }) => {
+      const compiled = await fetch(`${COMPILER_URL}/check`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
         },
-        {
-          body: t.Object({
-            code: t.String(),
-          }),
-          response: t.Union([
-            t.Object({ status: t.Literal("ok") }),
-            t.Object({
-              status: t.Literal("error"),
-              errors: t.Array(t.Any()),
-            }),
-          ]),
-          detail: { tags: ["api"] },
-        },
-      )
-      .get(
-        "/documents",
-        async ({ log, set, userId }): Promise<Document[]> => {
-          const documents = sql.getDocuments(userId);
-          set.status = 200;
-          return documents;
-        },
-        { detail: { tags: ["api"] } },
-      )
-      .post(
-        "/test",
-        async ({ log, set, body: { code }, userId }) => {
-          const compiled = await fetch(`${COMPILER_URL}/test`, {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              source: code,
-            }),
-          });
-          const data = await compiled.json();
+        body: JSON.stringify({
+          source: code,
+          user_id: userId,
+        }),
+      });
 
-          if (!compiled.ok) {
-            set.status = compiled.status;
-            log.error(`${data.error}`);
-            set.status = 400;
-            return data.error;
-          }
+      const data = await compiled.json();
 
-          set.status = 200;
-          log.info(`Data ${data} compiled.`);
-          return {
-            compiled: data,
-            user_id: userId,
-          };
+      if (!compiled.ok || data.status == "error") {
+        log.error(`${data.error}`);
+        return data;
+      }
+
+      set.status = 200;
+      log.info(`Data ${data} compiled.`);
+      return data;
+    },
+    {
+      body: t.Object({
+        code: t.String(),
+      }),
+      beforeHandle: authMiddleware,
+      response: t.Union([
+        t.Object({ status: t.Literal("ok") }),
+        t.Object({
+          status: t.Literal("error"),
+          errors: t.Array(t.Any()),
+        }),
+      ]),
+      detail: { tags: ["api"] },
+    },
+  )
+  .get(
+    "/documents",
+    async ({ log, set, userId }): Promise<Document[]> => {
+      const documents = sql.getDocuments(userId);
+      set.status = 200;
+      return documents;
+    },
+    {
+      beforeHandle: authMiddleware,
+      detail: { tags: ["api"] },
+    },
+  )
+  .post(
+    "/test",
+    async ({ log, set, body: { code }, userId }) => {
+      const compiled = await fetch(`${COMPILER_URL}/test`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
         },
-        {
-          body: t.Object({
-            code: t.String(),
-          }),
-          detail: { tags: ["api"] },
-        },
-      ),
+        body: JSON.stringify({
+          source: code,
+        }),
+      });
+      const data = await compiled.json();
+
+      if (!compiled.ok) {
+        set.status = compiled.status;
+        log.error(`${data.error}`);
+        set.status = 400;
+        return data.error;
+      }
+
+      set.status = 200;
+      log.info(`Data ${data} compiled.`);
+      return {
+        compiled: data,
+        user_id: userId,
+      };
+    },
+    {
+      body: t.Object({
+        code: t.String(),
+      }),
+      beforeHandle: authMiddleware,
+      detail: { tags: ["api"] },
+    },
   );
