4c302dd7cb1070796d2c9e48ea0738fa51474dba
Lunic Dumitru <dumitrulunik@gmail.com>
2024-08-27 12:57:59 +0200

    Protected routes from non authenticated persons

--- client/src/components/DRoutes.tsx
+++ client/src/components/DRoutes.tsx
@@ -1,4 +1,4 @@
-import { Router, Route, Navigate } from "@solidjs/router";
+import { Router, Route, Navigate, useNavigate } from "@solidjs/router";
 import { type Component } from "solid-js";
 import Home from "@client/routes/Home";
 import About from "@client/routes/About";
@@ -8,16 +8,35 @@ import Users from "@client/routes/Users";
 import UsersCreate from "@client/routes/UsersCreate";
 import UsersEdit from "@client/routes/UsersEdit";
 
+import authStore from "@store/authStore";
+
+
+interface ProtectedRouteProps {
+  component: Component;
+}
+
+const ProtectedRoute: Component<ProtectedRouteProps> = (props) => {
+  const navigate = useNavigate();
+  const isAuthenticated = authStore.state().isAuthenticated;
+
+  if (!isAuthenticated) {
+    navigate("/sign-in");
+    return null;
+  }
+
+  return <props.component />;
+};
+
 const DRoutes: Component = () => {
   return (
     <>
       <Router>
         <Route path="/sign-in" component={SignIn} />
-        <Route path="/editor" component={Editor} />
-        <Route path="/about" component={About} />
-        <Route path="/users" component={Users} />
-        <Route path="/users/create" component={UsersCreate} />
-        <Route path="/users/edit/:userId" component={UsersEdit} />
+        <Route path="/editor" component={() => <ProtectedRoute component={Editor} />} />
+        <Route path="/about" component={() => <ProtectedRoute component={About} />} />
+        <Route path="/users" component={() => <ProtectedRoute component={Users} />} />
+        <Route path="/users/create" component={() => <ProtectedRoute component={UsersCreate} />} />
+        <Route path="/users/edit/:userId" component={() => <ProtectedRoute component={UsersEdit} />} />
         <Route path="/home" component={Home} />
         <Route path="*" component={() => <Navigate href="/home" />} />
       </Router>
