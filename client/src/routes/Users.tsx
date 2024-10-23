beb4dd06a93f5a88eb7bdd1da860d48e31330df5
Lunic Dumitru <dumitrulunik@gmail.com>
2024-10-23 12:49:48 +0200

    fix: removed solid-contextmenu library, instead use kobalte, fixed buttons layout in admin dashboard

--- client/src/routes/Users.tsx
+++ client/src/routes/Users.tsx
@@ -1,7 +1,7 @@
 import { Component, createSignal, onMount, createEffect } from "solid-js";
 
 import { FaSolidEllipsis } from "solid-icons/fa";
-import { Menu, Item, useContextMenu, animation, Separator } from "solid-contextmenu";
+import { ContextMenu } from "@kobalte/core/context-menu";
 import { useNavigate } from "@solidjs/router";
 import Swal from "sweetalert2";
 
@@ -10,11 +10,9 @@ import { handleUnauthorized } from "@client/utils/authUtils";
 import Header from "@client/components/Header";
 import authStore from "@store/authStore";
 import styles from "@styles/Users.module.css";
-import { theme } from "@store/index";
+import menu_styles from "@styles/ContextMenu.module.css";
 import { UserDetails } from "@server/types";
 import { panic } from "@utils/panic";
-
-import "../../../node_modules/solid-contextmenu/dist/style.css";
 
 interface UsersResponse {
   data: UserDetails[] | { error: string } | null;
@@ -214,8 +212,9 @@ const Users: Component = () => {
     setReload(!reload());
   }
 
-  const [_animation] = createSignal(animation.scale);
+  // const [_animation] = createSignal(animation.scale);
   // const [_theme, setTheme] = createSignal<"light" | "dark">("light");
+
 
   return (
     <Header>
@@ -257,7 +256,7 @@ const Users: Component = () => {
 
               <tbody>
                 {filteredUsers().map((user) => {
-                  const { show } = useContextMenu({ id: user.id });
+                  // const { show } = useContextMenu({ id: user.id });
                   // const onlineStatus = onlineStatuses()[user.id];
                   let onlineStatus = false;
                   if (!user.lastTimeActive) {
@@ -286,36 +285,36 @@ const Users: Component = () => {
                       <td>{user.organization?.name}</td>
                       <td>{user.userRole}</td>
                       <td>{user.deleted ? "🗑️" : onlineStatus ? "🟢" : "🔴"}</td>
-                      <td
-                        onClick={(e) => {
-                          show(e, { props: user.id });
-                        }}
-                        class={styles.actions}
-                      >
-                        <FaSolidEllipsis />
-                        <Menu id={user.id} animation={_animation()} theme={theme() === "dark" ? "dark" : "light"}>
-                          <Item onClick={() => handleEditUser(user.id)} disabled={user.userRole == "Administrator"}>
-                            ✏️ Edit
-                          </Item>
-                          <Item
-                            onClick={() => handleDeleteUser(user.id)}
-                            disabled={user.userRole === "Administrator" || user.deleted}
-                          >
-                            🗑️ Delete
-                          </Item>
-                          <Separator />
-                          <Item
-                            onClick={() => handleLogOutUser(user.id, user.username)}
-                            disabled={
-                              user.userRole === "Administrator" ||
-                              !onlineStatus ||
-                              user.needsToBeLoggedOut ||
-                              user.deleted
-                            }
-                          >
-                            🚶 Log out
-                          </Item>
-                        </Menu>
+                      <td class={menu_styles.actions}>
+                        <ContextMenu>
+                          <ContextMenu.Trigger>
+                            <FaSolidEllipsis />
+                          </ContextMenu.Trigger>
+                          <ContextMenu.Content class={menu_styles["context-menu__content"]}>
+                            <ContextMenu.Item
+                              class={menu_styles["context-menu__item"]}
+                              onSelect={() => handleEditUser(user.id)}
+                              disabled={user.userRole == "Administrator"}
+                            >
+                              ✏️ Edit
+                            </ContextMenu.Item>
+                            <ContextMenu.Item
+                              class={menu_styles["context-menu__item"]}
+                              onSelect={() => handleDeleteUser(user.id)}
+                              disabled={user.userRole === "Administrator" || user.deleted}
+                            >
+                              🗑️ Delete
+                            </ContextMenu.Item>
+                            <ContextMenu.Separator />
+                            <ContextMenu.Item
+                              class={menu_styles["context-menu__item"]}
+                              onSelect={() => handleLogOutUser(user.id, user.name)}
+                              disabled={!onlineStatus}
+                            >
+                              🔒 Log out
+                            </ContextMenu.Item>
+                          </ContextMenu.Content>
+                        </ContextMenu>
                       </td>
                     </tr>
                   );
