4326261d5efca6034420edefc04dde33fa6b97c6
Lunic Dumitru <dumitrulunik@gmail.com>
2025-02-05 13:37:48 +0100

    chore: changed styling of dropdown menu

--- client/src/routes/Editor.tsx
+++ client/src/routes/Editor.tsx
@@ -326,7 +326,7 @@ function Editor(): JSX.Element {
               </Button.Root>
               <DropdownMenu>
                 <DropdownMenu.Trigger class={styles["dropdown-menu__trigger"]}>
-                  <span>Save as</span>
+                  <span class={styles["dropdown-menu__trigger_text"]}>Save as</span>
                 </DropdownMenu.Trigger>
                 <DropdownMenu.Portal>
                   <DropdownMenu.Content class={styles["dropdown-menu__content"]}>
