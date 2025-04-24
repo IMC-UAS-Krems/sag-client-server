64f3bf577b47131302468cfc718aa253ef370dfe
dfoddav2 <david.fodor.work@gmail.com>
2025-04-24 14:58:20 +0200

    chore: updated Prisma to make it up to date with `main` branch

--- client/src/guard/authGuard.tsx
+++ client/src/guard/authGuard.tsx
@@ -1,9 +1,9 @@
 import { createSignal, onMount, Show, onCleanup } from "solid-js";
 import { useNavigate } from "@solidjs/router";
 
-import { panic } from "@utils/panic";
-import authStore from "@store/authStore";
-import { eden } from "@client/api";
+import { panic } from "@utils/panic.ts";
+import authStore from "@store/authStore.ts";
+import { eden } from "@client/api/index.ts";
 
 const checkInterval =
   Number(import.meta.env.VITE_AUTH_CHECK_INTERVAL) * 1000 ||
